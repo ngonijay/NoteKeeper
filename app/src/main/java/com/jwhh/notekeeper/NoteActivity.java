f1a3db0c62fb0fbc3aa177860a37f4cf726b3095
ngoni jambo <ngonidzashe.jambo@cassavasmartech.co.zw>
2021-05-04 11:25:02 +0200

    add: restore Note previous values

--- app/src/main/java/com/jwhh/notekeeper/NoteActivity.java
+++ app/src/main/java/com/jwhh/notekeeper/NoteActivity.java
@@ -24,6 +24,9 @@ public class NoteActivity extends AppCompatActivity {
     private EditText mTextNoteText;
     private int mNotePosition;
     private boolean mIsCancelling;
+    private String mOriginalNoteCourseId;
+    private String mOriginalNoteTitle;
+    private String mOriginalNoteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +43,20 @@ public class NoteActivity extends AppCompatActivity {
         mCoursesSpinner.setAdapter(adapterCourse);
 
         readDisplayStateValues();
+        saveOriginalNoteValues();
+
         mTextNoteTitle = findViewById(R.id.text_note_title);
         mTextNoteText = findViewById(R.id.text_note_text);
         if (!mIsNewNote)
             displayNote(mCoursesSpinner, mTextNoteTitle, mTextNoteText);
+    }
+
+    private void saveOriginalNoteValues() {
+        if (mIsNewNote)
+            return;
+        mOriginalNoteCourseId = mNote.getCourse().getCourseId();
+        mOriginalNoteTitle = mNote.getTitle();
+        mOriginalNoteText = mNote.getText();
     }
 
     @Override
@@ -52,9 +65,19 @@ public class NoteActivity extends AppCompatActivity {
         if (mIsCancelling){
             if (mIsNewNote)
            DataManager.getInstance().removeNote(mNotePosition);
+            else {
+                storePreviousNoteValues();
+            }
         }else {
             saveNote();
         }
+    }
+
+    private void storePreviousNoteValues() {
+        CourseInfo course = DataManager.getInstance().getCourse(mOriginalNoteCourseId);
+        mNote.setCourse(course);
+        mNote.setTitle(mOriginalNoteTitle);
+        mNote.setText(mOriginalNoteText);
     }
 
     private void saveNote() {
